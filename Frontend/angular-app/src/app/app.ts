6c70a7182d2efd341356489e12d89134aefca70a
Jo1234-5 <johannes1.ellmer@sbg.at>
2025-08-01 11:50:46 +0200

    erstellung und implementierung des User und guthabenverwaltungs sytems frontend + Overview Bestellungen

--- Frontend/angular-app/src/app/app.ts
+++ Frontend/angular-app/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   template: '<router-outlet></router-outlet>',
   styleUrls: ['./app.css']
 })
