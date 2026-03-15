243ce6b232cca7188c02c3c92a3d1c3afeea957d
Jo1234-5 <145123413+Jo1234-5@users.noreply.github.com>
2026-03-15 19:35:50 +0100

    Hinzufügen von PWA Features

--- Frontend/angular-app/src/app/app.config.ts
+++ Frontend/angular-app/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
+  provideZoneChangeDetection, isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {
@@ -15,6 +15,7 @@ import { routes } from './app.routes';
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { PolicyRedirectInterceptor } from './auth/policy-redirect.interceptor';
 import { AutoLogoutInterceptor } from './auth/auth.logout.interceptor';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -28,6 +29,12 @@ export const appConfig: ApplicationConfig = {
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: PolicyRedirectInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: AutoLogoutInterceptor, multi: true },
-    provideCharts(withDefaultRegisterables()),
+    provideCharts(withDefaultRegisterables()), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
